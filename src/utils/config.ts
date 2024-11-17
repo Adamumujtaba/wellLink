5c28a8559d3d8bed12f1a88f2783d47ed7d6c3a8
Mujtaba <mujtaba.adamu@flexisaf.com>
2024-11-17 13:28:37 +0100

    fixed env location

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,4 @@
 const isDevelopment = import.meta.env.MODE === "development";
-
 export const BASE_URL = isDevelopment ? "http://localhost:3000" : import.meta.env.VITE_BASEURL;
 
 // If VITE_BASEURL is not set in production, throw an error
